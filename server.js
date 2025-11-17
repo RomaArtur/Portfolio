c10282b4646a250e696d1f8279113a27cd3db074
RomaArtur <arturromafurtado@gmail.com>
2025-11-17 19:52:45 -0300

    Corrige a importação do PrismaClient e ajusta a formatação do código no server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
-const { PrismaClient } = new("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const prisma = new PrismaClient(); 
+const prisma = new PrismaClient();
 const PORT = 4000;
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,7 +12,19 @@ app.get("/", (req, res) => {
   res.send("Back-end funcionando.");
 });
 
+app.get("/api/profile", async (req, res) => {
+  try {
+    const profile = await prisma.profile.findFirst();
+
+    res.json(profile);
+  } catch (error) {
+    console.error("Erro ao buscar perfil:", error);
+    res
+      .status(500)
+      .json({ error: "Não foi possível buscar os dados do perfil." });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
